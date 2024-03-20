a115961dddded5c8118a48af5cae1f9d0b9cca09
Samuelisusername <115652284+Samuelisusername@users.noreply.github.com>
2024-03-20 14:52:09 +0100

    Create solution.py

---
+++ Task1a/solution.py
@@ -0,0 +1,52 @@
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from  sklearn.linear_model._ridge import Ridge
+from sklearn.model_selection import cross_val_score
+
+
+
+
+def average_LR_RMSE(X, y, lambdas, n_folds):
+    """
+    Main cross-validation loop, implementing 10-fold CV. In every iteration (for every train-test split), the RMSE for every lambda is calculated, 
+    and then averaged over iterations.
+    
+    Parameters
+    ---------- 
+    X: matrix of floats, dim = (150, 13), inputs with 13 features
+    y: array of floats, dim = (150, ), input labels
+    lambdas: list of floats, len = 5, values of lambda for which ridge regression is fitted and RMSE estimated
+    n_folds: int, number of folds (pieces in which we split the dataset), parameter K in KFold CV
+    
+    Returns
+    ----------
+    avg_RMSE: array of floats: dim = (5,), average RMSE value for every lambda
+    """
+    RMSE_mat = np.zeros((n_folds, len(lambdas)))
+    for i in range(len(lambdas)):
+        model = Ridge(alpha=lambdas[i], fit_intercept=False)
+        RMSE_mat[:,i] = -1 * cross_val_score(model, X, y, cv=n_folds, scoring='neg_root_mean_squared_error')
+        print("this is the lamda ", lambdas[i], np.average(RMSE_mat[:,i]))
+
+    avg_RMSE = np.mean(RMSE_mat, axis=0)
+    assert avg_RMSE.shape == (5,)
+    return avg_RMSE
+
+
+if __name__ == "__main__":
+    # Data loading
+    data = pd.read_csv("train.csv")
+    y = data["y"].to_numpy()
+    data = data.drop(columns="y")
+    # print a few data samples
+    print(data.head())
+
+    X = data.to_numpy()
+    # The function calculating the average RMSE
+    lambdas = [0.1, 1, 10, 100, 200]
+    n_folds = 10
+    avg_RMSE = average_LR_RMSE(X, y, lambdas, n_folds)
+    # Save results in the required format
+    np.savetxt("./results.csv", avg_RMSE, fmt="%.12f")
